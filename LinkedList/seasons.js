9098116046ffe2e1df988b555fc463653f5c58fa
pedroLSoares <pedrohenrique.levada@gmail.com>
2022-01-16 01:25:01 -0300

    Add node and linked list

---
+++ LinkedList/seasons.js
@@ -0,0 +1,13 @@
+const LinkedList = require('./LinkedList');
+
+const seasons = new LinkedList();
+
+seasons.addToHead('summer');
+seasons.addToHead('spring');
+
+seasons.addToTail('fall');
+seasons.addToTail('winter');
+
+seasons.removeHead();
+
+seasons.printList();
